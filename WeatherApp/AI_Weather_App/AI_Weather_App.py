95b4ea0d9be75be8db4581ace34af7372a6e7656
unknown <smhamzamehdi97@gmail.com>
2024-12-12 17:30:30 +0900

    New Update

---
+++ WeatherApp/AI_Weather_App/AI_Weather_App.py
@@ -0,0 +1,23 @@
+#Welcome to Weather Application using Python
+#This App would contain a complete weather forecast with integration of AI based suggestions regarding weather
+#Lets begin
+
+
+
+
+
+def main():
+    """
+
+    """
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    main()
